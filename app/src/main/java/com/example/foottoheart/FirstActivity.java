717427af7ef90d4f9f8660085901a600bd10f32e
choicheoulwon <choicheoulwon@gmail.com>
2019-05-19 21:00:25 +0900

    최초,로그인,회원가입 activity 틀 구현

---
+++ app/src/main/java/com/example/foottoheart/FirstActivity.java
@@ -0,0 +1,40 @@
+package com.example.foottoheart;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    Button btSignin,btSignup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        //로그인 버튼 눌렀을 때 로그인 화면으로 이동
+        btSignin = (Button)findViewById(R.id.firstactivity_button_signin);
+        btSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signin_intent = new Intent(getApplicationContext(),SigninActivity.class);
+                startActivity(signin_intent);
+
+            }
+        });
+
+        btSignup = (Button)findViewById(R.id.firstactivity_button_signup);
+        btSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup_intet = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(signup_intet);
+            }
+        });
+
+
+
+    }
+}
