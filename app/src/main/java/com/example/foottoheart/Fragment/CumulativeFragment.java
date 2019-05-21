cd7bea732394696bfcf0cfbac089a77ac544cff3
choicheoulwon <choicheoulwon@gmail.com>
2019-05-22 03:54:43 +0900

    네비게이션 바 아이콘 추가

--- app/src/main/java/com/example/foottoheart/Fragment/CumulativeFragment.java
+++ app/src/main/java/com/example/foottoheart/Fragment/CumulativeFragment.java
@@ -7,7 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.style.ForegroundColorSpan;
 
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
