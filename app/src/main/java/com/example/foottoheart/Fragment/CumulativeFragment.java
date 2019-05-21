a5197ffb730e88bc102dbee85789dd02cc8eb739
choicheoulwon <choicheoulwon@gmail.com>
2019-05-22 02:16:50 +0900

    홈 화면, 누적 화면 구현

--- app/src/main/java/com/example/foottoheart/Fragment/CumulativeFragment.java
+++ app/src/main/java/com/example/foottoheart/Fragment/CumulativeFragment.java
@@ -1,20 +1,104 @@
 package com.example.foottoheart.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.style.ForegroundColorSpan;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.foottoheart.R;
+import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+
 
 public class CumulativeFragment extends Fragment {
+
+    MaterialCalendarView materialCalendarView;
+    ArcProgress arcProgress;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_cumulative,container,false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        arcProgress = (ArcProgress)getView().findViewById(R.id.fragmentcumulative_arcprogress);
+
+        materialCalendarView = (MaterialCalendarView)getView().findViewById(R.id.fragmentcumulative_calendar);
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator()
+                );
+
+
+
+        materialCalendarView.setDateSelected(CalendarDay.today(),true);
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                //임시
+                arcProgress.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+    public class SundayDecorator implements DayViewDecorator {
+
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SundayDecorator() {
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SUNDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(Color.RED));
+        }
+    }
+
+    public class SaturdayDecorator implements DayViewDecorator {
+
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SaturdayDecorator() {
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SATURDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(Color.BLUE));
+        }
+    }
 }
+
--- app/src/main/java/com/example/foottoheart/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/foottoheart/Fragment/HomeFragment.java
@@ -7,11 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.foottoheart.R;
+import com.github.lzyzsd.circleprogress.ArcProgress;
 
 public class HomeFragment extends Fragment {
 
+    int count = 0;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,5 +22,20 @@ public class HomeFragment extends Fragment {
 
         return inflater.inflate(R.layout.fragmet_home,container,false);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button controller = (Button)getView().findViewById(R.id.controller);
+        final ArcProgress arcProgress = (ArcProgress)getView().findViewById(R.id.fragmenthome_arcprogress);
+        controller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                arcProgress.setProgress(count);
+                arcProgress.setBottomText("달성도 : "+ (int)(count/(float)arcProgress.getMax()*100) + "%");
+            }
+        });
     }
 }
